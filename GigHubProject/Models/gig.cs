f190a852512910c480a89ec559836ccaa468d5bf
0x5nc <slim.essency@gmail.com>
2018-05-04 17:44:43 +0200

    Create the Gig model.

---
+++ GigHubProject/Models/gig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHubProject.Models
+{
+    public class gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+    }
+}
