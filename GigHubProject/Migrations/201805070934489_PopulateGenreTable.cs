1b999d9443420c55d8a7fbd73b336952a6c4110a
0x5nc <slim.essency@gmail.com>
2018-05-07 11:50:23 +0200

    Populate Genres table.

---
+++ GigHubProject/Migrations/201805070934489_PopulateGenreTable.cs
@@ -0,0 +1,21 @@
+namespace GigHubProject.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class PopulateGenreTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("SET IDENTITY_INSERT Genres ON");
+            Sql("Insert into Genres(Id, Name) values (1, 'Jazz')");
+            Sql("Insert into Genres(Id, Name) values (2, 'Blues')");
+            Sql("Insert into Genres(Id, Name) values (3, 'Rock')");
+            Sql("Insert into Genres(Id, Name) values (4, 'Country')");
+        }
+
+        public override void Down()
+        {
+            Sql("Delete from Genres where Id in (1,2,3,4)");
+        }
+    }
+}
